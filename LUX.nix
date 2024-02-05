f9df9adbb5557d5b3b201055d17d1a39ed04d822
NikolayTach <42199859+NikolayTach@users.noreply.github.com>
2024-02-04 22:07:19 -0500

    Update LUX.nix

--- LUX.nix
+++ LUX.nix
@@ -1,7 +1,8 @@
 {nix, pkg, config, ...}
-
-{insert.pid= nix/store/iwgkck1d64s5f26ngln540hpf06g28py-nix-2.20.1/lib
 {
+    insert.pid= nix/store/iwgkck1d64s5f26ngln540hpf06g28py-nix-2.20.1/lib
+      {
 log user = [nix_log01; pid1,nix_log02; pid2 nix.log03, pid3]
-pkg.config.default as (main0)
+pkg.config.default as main();
+      }
 }
